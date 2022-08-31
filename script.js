1465218797df206b5dcc29e8f5635a9249d3f678
Christian <c.waldschmidt11@gmail.com>
2022-08-30 22:56:14 -0500

    Add calculator JavaScript class

---
+++ script.js
@@ -0,0 +1,48 @@
+class Calculator {
+    constructor(preNumTextElement, curNumTextElement) {
+        this.preNumTextElement = preNumTextElement
+        this.curNumTextElement = curNumTextElement
+        this.clear()
+    }
+
+    clear() {
+        this.currentNumber = ''
+        this.previousNumber = ''
+        this.operation = undefined
+    }
+
+    updateNumber(number) {
+        this.currentNumber = number
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.curNumTextElement.innertext = this.currentNumber 
+    }
+}
+
+
+const numberBtns = document.querySelectorAll('[data-number]')
+const operationBtns = document.querySelectorAll('[data-operation]')
+const equalsBtn = document.querySelector('[data-equals]')
+const clearBtn = document.querySelector('[data-clear]')
+const percentBtn = document.querySelector('[data-percent]')
+const signChangeBtn = document.querySelector('[data-signChange]')
+const preNumTextElement = document.querySelector('[data-previousNumber]')
+const curNumTextElement = document.querySelector('[data-currentNumber]')
+
+const calculator = new Calculator(preNumTextElement, curNumTextElement)
+
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.updateNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
